a8889248afe71bda40efc6683e3246b183b3a567
Nii Nartey <nii.tetteh@amalitech.com>
2025-04-07 18:56:24 +0000

    add getUpdates and other fields

--- src/main/java/org/emailservice/SesHandler.java
+++ src/main/java/org/emailservice/SesHandler.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 public class SesHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
 
@@ -12,22 +14,48 @@ public class SesHandler implements RequestHandler<Map<String, String>, Map<Strin
     public Map<String, String> handleRequest(final Map<String, String> input, Context context) {
 
 
-        String key1 = input.get("name");
-        String key2 = input.get("email");
-        String key3 = input.get("message");
-        String key4 = input.get("inquiryType");
+        try {
+            String firstName = input.get("firstName");
+            String lastName = input.get("lastName");
+            String email = input.get("workEmail");
+            String company = input.get("company");
+            String country = input.get("country");
+            String message = input.get("message");
+            String inquiryType = input.get("inquiryType");
+            String getUpdates;
+
+            if (input.get("getUpdates") == null) {
+                getUpdates = "false";
+            } else if (Objects.equals(input.get("getUpdates"), "true")) {
+                getUpdates = "true";
+            } else {
+                getUpdates = "false";
+            }
 
 
-        // Set up response
-        Map<String, String> response = new HashMap<>();
-        response.put("name", key1);
-        response.put("message", key2);
-        response.put("email", key3);
-        response.put("inquiryType", key4);
+            context.getLogger().log("firstName: [" + firstName + " ]");
+            context.getLogger().log("lastName: [" + lastName + " ]");
+            context.getLogger().log("email: [" + email + " ]");
+            context.getLogger().log("company: [" + company + " ]");
+            context.getLogger().log("country: [" + country + " ]");
+            context.getLogger().log("message: [" + message + " ]");
+            context.getLogger().log("inquiryType: [" + inquiryType + " ]");
+            context.getLogger().log("getUpdates: [" + getUpdates + " ]");
 
-        context.getLogger().log("response: [" + response + " ]");
 
-        return response;
+            // Set up response
+            Map<String, String> response = new HashMap<>();
+            response.put("name", firstName + " " + lastName);
+            response.put("message", message);
+            response.put("email", email);
+            response.put("inquiryType", inquiryType);
+            response.put("id", UUID.randomUUID().toString());
+
+            context.getLogger().log("response: [" + response + " ]");
+
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
