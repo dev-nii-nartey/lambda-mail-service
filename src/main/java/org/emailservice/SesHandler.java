ef3b4580f1cbd21d8c5be9a3d55c4415eaf42d3f
Nii Nartey <nii.tetteh@amalitech.com>
2025-04-07 21:12:08 +0000

    strip escaped data

--- src/main/java/org/emailservice/SesHandler.java
+++ src/main/java/org/emailservice/SesHandler.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sesv2.SesV2Client;
 import software.amazon.awssdk.services.sesv2.model.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,38 +15,40 @@ import java.util.UUID;
 
 public class SesHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
 
-    private static final String SENDER_EMAIL = "nii.tetteh@amalitech.com"; // Replace with your verified SES email
+    private static final String SENDER_EMAIL = "nii.tetteh@amalitech.com";
     private static final String TEMPLATE_NAME = "ContactFormAcknowledgement";
     private static final Region AWS_REGION = Region.EU_WEST_1;
+    private static final Gson gson = new Gson();
 
     @Override
     public Map<String, String> handleRequest(final Map<String, String> input, Context context) {
         try {
-            String firstName = input.get("firstName");
-            String lastName = input.get("lastName");
-            String email = input.get("workEmail");
-            String company = input.get("company");
-            String country = input.get("country");
-            String message = input.get("message");
-            String inquiryType = input.get("inquiryType");
+            // Get values from input and strip any wrapping quotes
+            String firstName = stripQuotes(input.get("firstName"));
+            String lastName = stripQuotes(input.get("lastName"));
+            String email = stripQuotes(input.get("email"));
+            String company = stripQuotes(input.get("company"));
+            String country = stripQuotes(input.get("country"));
+            String message = stripQuotes(input.get("message"));
+            String inquiryType = stripQuotes(input.get("inquiryType"));
             String getUpdates;
 
             if (input.get("getUpdates") == null) {
                 getUpdates = "false";
-            } else if (Objects.equals(input.get("getUpdates"), "true")) {
+            } else if (Objects.equals(stripQuotes(input.get("getUpdates")), "true")) {
                 getUpdates = "true";
             } else {
                 getUpdates = "false";
             }
 
-            context.getLogger().log("firstName: [" + firstName + " ]");
-            context.getLogger().log("lastName: [" + lastName + " ]");
-            context.getLogger().log("email: [" + email + " ]");
-            context.getLogger().log("company: [" + company + " ]");
-            context.getLogger().log("country: [" + country + " ]");
-            context.getLogger().log("message: [" + message + " ]");
-            context.getLogger().log("inquiryType: [" + inquiryType + " ]");
-            context.getLogger().log("getUpdates: [" + getUpdates + " ]");
+            context.getLogger().log("firstName: [" + firstName + "]");
+            context.getLogger().log("lastName: [" + lastName + "]");
+            context.getLogger().log("email: [" + email + "]");
+            context.getLogger().log("company: [" + company + "]");
+            context.getLogger().log("country: [" + country + "]");
+            context.getLogger().log("message: [" + message + "]");
+            context.getLogger().log("inquiryType: [" + inquiryType + "]");
+            context.getLogger().log("getUpdates: [" + getUpdates + "]");
 
             String contactId = UUID.randomUUID().toString();
 
@@ -59,13 +63,18 @@ public class SesHandler implements RequestHandler<Map<String, String>, Map<Strin
             response.put("inquiryType", inquiryType);
             response.put("id", contactId);
 
-            context.getLogger().log("response: [" + response + " ]");
+            context.getLogger().log("response: [" + response + "]");
 
             return response;
         } catch (Exception e) {
             context.getLogger().log("Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    private String stripQuotes(String value) {
+        if (value == null) return "";
+        return value.replaceAll("^\"|\"$", "");
     }
 
     private void sendAcknowledgementEmail(String firstName, String lastName, String recipientEmail,
@@ -81,21 +90,19 @@ public class SesHandler implements RequestHandler<Map<String, String>, Map<Strin
                     .toAddresses(recipientEmail)
                     .build();
 
-            // Prepare template data - all variables that will be used in the template
-            String templateData = String.format(
-                    "{"
-                            + "\"firstName\": \"%s\","
-                            + "\"lastName\": \"%s\","
-                            + "\"fullName\": \"%s %s\","
-                            + "\"company\": \"%s\","
-                            + "\"inquiryType\": \"%s\","
-                            + "\"message\": \"%s\","
-                            + "\"contactId\": \"%s\","
-                            + "\"currentDate\": \"%s\""
-                            + "}",
-                    firstName, lastName, firstName, lastName, company, inquiryType,
-                    message.replace("\"", "\\\"").replace("\n", "\\n"),
-                    contactId, java.time.LocalDate.now());
+            // Use Gson to create proper JSON for template data
+            JsonObject templateDataObj = new JsonObject();
+            templateDataObj.addProperty("firstName", firstName);
+            templateDataObj.addProperty("lastName", lastName);
+            templateDataObj.addProperty("fullName", firstName + " " + lastName);
+            templateDataObj.addProperty("company", company);
+            templateDataObj.addProperty("inquiryType", inquiryType);
+            templateDataObj.addProperty("message", message);
+            templateDataObj.addProperty("contactId", contactId);
+            templateDataObj.addProperty("currentDate", java.time.LocalDate.now().toString());
+
+            String templateData = gson.toJson(templateDataObj);
+            context.getLogger().log("Template data: " + templateData);
 
             // Create the template email content
             Template template = Template.builder()
